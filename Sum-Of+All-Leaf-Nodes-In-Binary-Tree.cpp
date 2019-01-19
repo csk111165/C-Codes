617823868e164160cbc4fe36bf9f1e08067d55ba
Chandra Shekhar <csk.111165@gmail.com>
2019-01-19 22:54:48 +0530

    Sum of all nodes in BINARY TREE

---
+++ Sum-Of+All-Leaf-Nodes-In-Binary-Tree.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left,*right;
+};
+
+bool IsLeafNode(Node* node)
+{
+    if(node == NULL)
+        return false;
+    if(node->left == NULL && node->right == NULL)
+        return true;
+
+    return false;//donot foget this condition otherwise it will consider the parent node as well
+}
+
+void FindLeafNodeSum(Node* node, int& sum)
+{
+
+    if(node == NULL)
+        return;
+    if(IsLeafNode(node))
+    {
+        sum += node->data;
+    }
+
+    FindLeafNodeSum(node->left,sum);
+    FindLeafNodeSum(node->right,sum);
+
+}
+Node* CreateNode(int data)
+{
+
+    Node* new_node = new Node;
+    new_node->data=data;
+    new_node->left = new_node->right = NULL;
+    return new_node;
+
+}
+
+int main()
+{
+    Node* root = NULL;
+    root = CreateNode(1);
+    root->left = CreateNode(2);
+    root->right = CreateNode(3);
+    root->left->left = CreateNode(4);
+    root->left->right = CreateNode(5);
+    root->right->left = CreateNode(6);
+    root->right->right = CreateNode(7);
+    int sum = 0 ;
+    FindLeafNodeSum(root,sum);
+cout<<"Sum of All Leaf Nodes Of Tree is : "<<sum;
+    return 0;
+}
